014d95bea1bb6c74c5be91e5b327c8425219ecc7
lindeyi@bjnja.com <lindeyi@bjnja.com>
2017-10-24 15:55:01 +0800

    package war

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -8,6 +8,7 @@ var CopyWebpackPlugin = require('copy-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+const { WebpackWarPlugin } = require('webpack-war-plugin')
 
 var env = config.build.env
 
@@ -90,7 +91,10 @@ var webpackConfig = merge(baseWebpackConfig, {
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
       }
-    ])
+    ]),
+    new WebpackWarPlugin({
+      archiveName: 'static'
+    })
   ]
 })
 
